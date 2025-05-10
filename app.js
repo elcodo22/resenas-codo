73afad2642e46d59341be9b61116a2644c466c45
Alejandro Codoñer Ros <125590777+elcodo22@users.noreply.github.com>
2025-05-11 01:50:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+const uploadFile = async () => {
+    const fileInput = document.getElementById('csvFile');
+    const file = fileInput.files[0];
+    if (!file) {
+        alert('Por favor, selecciona un archivo CSV');
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('csv', file);
+
+    try {
+        const response = await fetch('http://35.177.116.70:3000/upload', {
+            method: 'POST',
+            body: formData,
+        });
+
+        const result = await response.json();
+        console.log("Respuesta del backend (subida):", result);
+
+        if (result.mensaje) {
+            alert(result.mensaje); // Confirmación
+        }
+
+    } catch (error) {
+        console.error('Error al subir el archivo:', error);
+        alert('Error al subir el archivo.');
+    }
+};
 const cargarResumenDesdeDynamo = async () => {
     try {
         const response = await fetch('http://35.177.116.70:3000/leer-dynamo');
